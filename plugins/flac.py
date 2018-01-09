bf2111ed8c5f6f9dc13adcc0f6379e3dbf32347f
Kayvan Javid <javid.kayvan@gmail.com>
2018-01-09 15:16:00 +0000

    Updated plugins/flac to return the md5 string correctly
    Was previously returning what looked like a massive number,
    instead of what it should be which is 16 bytes worth of hex.
    
    Added lots of commentary regarding the use of mutagen to read
    the flac STREAMINFO header and parsing it appropriately.
    
    Signed-off-by: Kayvan Javid <javid.kayvan@gmail.com>

--- plugins/flac.py
+++ plugins/flac.py
@@ -9,16 +9,30 @@ def md5( filename ):
     md5 = None
     try:
         audiofile = FLAC( filename )
-        md5 = str(audiofile.info.md5_signature)
+        # mutagen.flac.FLAC.info() provides access to all content
+        # from the STREAMINFO block in flac header.  The docs do not
+        # mention md5_signature specifically, but looking at the content
+        # of the StreamInfo object returned by the info() method it
+        # contains an md5_signature attribute.
+        # print( vars( audiofile ) )
+        # See: http://mutagen.readthedocs.io/en/latest/api/flac.html?highlight=FLAC
+        #
+        # The value stored in the STEAMINFO block is a 16 byte field hex field.
+        # This gets decoded using the format string %02x
+        # Zero padding seems to be getting stripped from formatted output,
+        # so fixed with an rjust() to zero pad upto the max length of an md5
+        # string of 32 characters.
+        md5 = ( "%02x" % audiofile.info.md5_signature ).rjust(32,'0')
     except mutagen.MutagenError as err:
         print("error")
         logging.error( 'Failed to open file: %s', err )
-    #if debug: print( vars( audiofile.info ) )
+#    print( vars( audiofile.info ) )
     logging.debug('get_flac_md5_signature( %s ) returning: %s', filename, str(md5))
 
     # Explicitly cast this to string so we do not end up with any ambiguity
-    # over it being a really long number. During db insertion, the Pythonic
-    # duck typing an sqlite column is subject to has resulted in problems with
-    # what looks like an md5sum value looking like a massive integer causing INT overflow errors
+    # over it being a really long number. During db insertion, the duck typing
+    # which an sqlite column is subject to, has resulted in problems with
+    # what looks like an md5sum value looking like a massive integer causing 
+    # integer overflow errors
     return md5
 
