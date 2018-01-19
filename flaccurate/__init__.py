8b40354b315385a06b4e08646ba75a19fd184f11
Kayvan Javid <javid.kayvan@gmail.com>
2018-01-19 21:28:01 +0000

    Update flaccurate/__init__ Database import
    Just for better consistency, there was no need to use a relative
    import .database notation.  Fully qualifying the import gives it
    consistency with the plugins import.
    
    Signed-off-by: Kayvan Javid <javid.kayvan@gmail.com>

--- flaccurate/__init__.py
+++ flaccurate/__init__.py
@@ -1,5 +1,5 @@
 # Avoid module and class double-barrelled importing by
 # importing the Classes in __init__.py so the point of use can simply be:
 # import Class
-from .database import Database
+from flaccurate.database import Database
 import flaccurate.plugins
