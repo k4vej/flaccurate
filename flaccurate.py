91358c910ec216f92aaaf11a1ade80665cdce67c
Kayvan Javid <javid.kayvan@gmail.com>
2018-01-24 20:23:15 +0000

    Significant rework of flaccurate implementation
    Out of a desire to make both a good CLI program, as well as structure
    the code in a modular fashion, allowing something other than a
    CLI to reuse the same implementation efficiently, have reorganised and
    reimplemented accordingly.
    
    Based on lessons and thoughts derived from:
    https://stormpath.com/blog/building-simple-cli-interfaces-in-python
    Which walks through a really well structured project skele-cli:
    https://github.com/rdegges/skele-cli)
    Which in turn uses docopt as a basis for building "beautiful command-line
    interfaces"
    
    Chose not to use docopt in the end.  Its automagic unification of
    docstring parsing, turning it into a working argument parsing spec is
    great.  Other quirky aspects like having an argument dictionary with
    "--" prefixes on everything not so much.
    
    Anyway, code:
     * Created cli.py entry point which performs the inital command line
       argument parsing, including the "sub-command" the user has
       requested, itterates through the flaccurate.commands modules
       imported, discovers the appropriate Class corresponding to the
       user supplied "sub-command" and invokes it's run().
     * Reworked "actions" performed by wrapping up each separate and
       distinct "action" into a module which can invoked by the user
       supplying the name of the module as a command.  At the time of
       writing only two actions have been ported from the original
       functionality:
       flaccurate.commands.selfcheck.SelfCheck() > cli.py selfcheck
       flaccurate.commands.noop.NoOp() > cli.py noop
     * Each command inherits from a parent class called Base.
       flaccurate.commands.base.Base()
     * Base() does contain the bulk of the boiler plate and generic
       implementation necessary for any derived command classes to do
       anything useful as flaccurate commands.  Logging, database
       initialisation.
     * Base() when instantiated doesn't do much else other than
       parse the supplied arguments, and stores them as attributes,
       so any derived class has access to the user supplied arguments
       and can take actions accordingly.
     * Implemented an exception module.  Currently it contains a
       single class Usage().  Inspired by Guido himself,
       See: http://www.artima.com/weblogs/viewpost.jsp?thread=4829
       This exception class is used by each of the command
       classes as a way of advertising information about themselves.
       Inherited from Base()
       cli.py --usage selfcheck > print self.__doc__
    
    Signed-off-by: Kayvan Javid <javid.kayvan@gmail.com>

--- flaccurate.py
+++ flaccurate.py
@@ -1,227 +1,102 @@
-import os
 import sys
-import glob
-import re
-import logging
 import argparse
-import importlib
-from pathlib import Path
 
-import filetype as filemagic
-import filetype.utils
+from inspect import getmembers, isclass
 
-import flaccurate
+def main():
+    """Main CLI entrypoint."""
+    import flaccurate.commands
+    import flaccurate
+    args = _init_argparse()
 
-def main(args=None):
-    # For all supported filetypes,
-    # recurse through the supplied path and determine the audio hash.
-    # A supported filetype is determined by the presence of a
-    # corresponding plugin for the file extension (see plugins folder).
-
-    # Operation as follows:
-    # For each supported file type (read: plugin) (see: process_filetype())
-    # For each matching file found in file type (see: itterate_iglob())
-    # Calculate the audio md5 checksum: (see: _process_file())
-    # 1. If the database record exists for file (see: Database._retrieve_checksum())
-    #       Compare new checksum against database record (see: _process_file())
-    #           If checksum matches all good
-    #           If checksum doesn't match report it
-    # 2. If the database has no record of file - insert it for first time (see: Database._insert_checksum())
-
-    Flaccurate().process_all()
-
-
-class Flaccurate:
-    PLUGINS_PATH = 'plugins'
-
-    def __init__(self):
-        self.args = self._init_argparse()
-        self.debug = self.args.debug
-        self.silent = self.args.silent
-        self.log_level = self._init_logging()
-        logging.debug('args: %s', vars(self.args))
-
-        self.db = self._init_database()
-
-        self.plugins = self._init_plugins()
-
-        # Perform no file processing - but quit after all
-        # initialisation and plugin loading
-        if(self.args.selfcheck):
-            logging.info('Self check complete - exiting...')
-            sys.exit(0)
-
-        if(self.args.path is None):
-            logging.info('No path specified - nothing TODO - exiting...')
-            # implement checking for config elsewhere (file / env variable)
-            sys.exit(0)
-        else:
-            if not(os.path.isdir(self.args.path)):
-                logging.info('Specified path does not exist - exiting')
-                sys.exit(0)
-
-
-    def _init_argparse(self):
-        # Note: most defaults are explicitly set to None
-        # Setting fallback defaults is instead delayed until after
-        # argparse is complete so that:
-        # 1. Detection and explicit logging of this fact
-        # 2. More complex decisions about what the default is can be done
-        # See: __init__()
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-            '-d', '--debug',
-            help='enable debug', action='store_true'
-        )
-        parser.add_argument(
-            '-s', '--silent',
-            help='silent mode - will only output warnings or errors', action='store_true'
-        )
-        parser.add_argument(
-            '-c', '--selfcheck',
-            help='performs all initialisation and plugin loading then exits', action='store_true'
-        )
-        parser.add_argument(
-            'path',
-            nargs='?',
-            type=str,
-            default=None,
-            help='specify directory where audio files are stored'
-        )
-        parser.add_argument(
-            '-db', '--database',
-            nargs='?',
-            type=str,
-            default=None,
-            help='specify the database file to use',
-        )
-        return parser.parse_args()
-
-    def _init_logging(self):
-        if( self.debug ):
-            log_level = 'debug'
-        elif( self.silent ):
-            log_level = 'warning'
-        else:
-            log_level = 'info'
-
-        LEVELS = {
-            'debug': logging.DEBUG,
-            'info': logging.INFO,
-            'warning': logging.WARNING,
-            'error': logging.ERROR,
-            'critical': logging.CRITICAL
-        }
-
-        logging.basicConfig(
-            level=LEVELS.get(
-                log_level,
-                logging.NOTSET
-            ),
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        )
-        return log_level
-
-    def _init_database(self):
-        db = None
-        try:
-            self.db = flaccurate.Database(self.args)
-        except RuntimeError as e:
-            logging.critical('%s - exiting', e.args[0])
-            sys.exit(1)
-        return db
-
-    def _init_plugins(self):
-        logging.debug('_init_plugins( %s )', self.PLUGINS_PATH)
-        # Inspired by importdir by Aurelien Lourot
-        # See: https://gitlab.com/aurelien-lourot/importdir
-        plugins = {}
-
-        for module_name, imported_module in sys.modules.items():
-            if( module_name.startswith( 'flaccurate.plugins.' ) ):
-                plugin_name = module_name.replace('flaccurate.plugins.','')
-                logging.debug('_init_plugins( %s ): Found plugin %s', self.PLUGINS_PATH, plugin_name)
-                plugins[plugin_name] = sys.modules[module_name]
-
-        return plugins
-
-    def _calculate_checksum(self, filename, filetype):
-        logging.debug('_calculate_checksum( %s, %s )', filename, filetype)
-        return self.plugins[filetype].md5(filename)
-
-    def _itterate_iglob(self, filetype):
-        logging.debug('itterate_iglob( %s )', filetype)
-        logging.info('Processing %s files', filetype)
-        count = 0
-        for filename in glob.iglob(self.args.path + '**/*.' + filetype, recursive=True):
-            logging.info('%s: %s', filetype, filename)
-            count += 1
-            self._process_file(filename, filetype)
-
-        logging.info('Processed %i %s files', count, filetype)
-
-    def _valid_file(self, filename, filetype):
-        logging.debug('_valid_file( %s, %s )', filename, filetype)
-        # Delegate file validation to the filetype module.
-        # Aliased to filemagic on import - already using a variable
-        # called filetype throughout.
-
-        # The filetype module does file magic checking in pure python,
-        # so no deps or bindings on libmagic or anything else.
-        # Interface is a bit clunky, considered duplicating its validation
-        # logic in each plugin, but placing the one very awkward call here
-        # means it will apply to every supported filetype (read: plugin)
-        # without any additional work.
-        #
-        # 1. Retrieve a filemagic object of the type we want
-        # to validate against.  Bit of a nightmare, as the function:
-        # get_type() compares the "filetype" argument passed in,
-        # against an enumerated list of filetypes, using is().
-        # is() does not play nicely if the filetype argument takes the form
-        # of a key from a dictionary - never matching against what looks like
-        # identical strings.  Need to jump through an intern() hoop to get the
-        # "correct string" for comparison.
-        # 2. Using the filemagic object returned from step one above, via:
-        # get_type() - call it's instance method: match() passing in the
-        # appropriate bytes from the file being tested - this is where the
-        # the magic happens..
-        # The public API is at least helpful here, providing a utility function:
-        # get_signature_bytes(filename)
-        # Return from match() is Boolean, so just pass it back to caller.
-        return filemagic.get_type(None,sys.intern(filetype)).match( filemagic.utils.get_signature_bytes( filename ) )
-
-    def _process_file(self, filename, filetype):
-        logging.debug('_process_file( %s, %s )', filename, filetype)
-
-        if(not self._valid_file(filename, filetype)):
-            logging.info('Skipping invalid %s: %s', filetype, filename)
-            return
-
-        checksum_calculated = self._calculate_checksum( filename, filetype )
-        checksum_record = self.db._retrieve_checksum( filename )
-
-        if( checksum_record is not None ):
-            if( checksum_calculated == checksum_record ):
-                logging.debug('_process_file( %s, %s ): Checksum verified', filename, filetype)
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+    # Inspired by: https://github.com/rdegges/skele-cli
+    for user_command in args.command:
+        if hasattr(flaccurate.commands, user_command):
+            module = getattr(flaccurate.commands, user_command)
+            flaccurate.commands = getmembers(module, isclass) # get class name
+            command = [command[1] for command in flaccurate.commands if command[0] != 'Base'][0]
+            try:
+                command = command(args) # instantiate
+            except flaccurate.Usage as e:
+                print(e)
             else:
-                logging.warning('%s: %s - Failed checksum (Current: %s Previous: %s)', filetype, filename, checksum_calculated, checksum_record)
-        else:
-            logging.debug('_process_file( %s, %s ): Inserting new checksum', filename, filetype)
-            self.db._insert_checksum({
-                'filename': filename,
-                'md5': checksum_calculated,
-                'filetype': filetype
-            })
+                command.run()
 
-    def supported_filetypes(self):
-        return self.plugins.keys()
+def _init_argparse():
+    # obtain list of commands found in flaccurate.commands module namespace
+    available_commands = _discover_commands()
 
-    def process_filetype(self, filetype):
-        self._itterate_iglob(filetype)
+    # Use this enumerated list of available_commands to populate argparse
+    # choices for the mandatory "command" positional parameter - and infom
+    # users of the available choices by printing them in the epilog for the
+    # help output.  This is done rather than statically populating the parser
+    # with sub-command help, as the point of the commands is their creation
+    # and presence are dynamically discovered at runtime.
+    epilog_text = 'commands available: ' + ', '.join(available_commands)
+    parser = argparse.ArgumentParser(epilog=epilog_text)
 
-    def process_all(self):
-        for filetype in self.supported_filetypes():
-            self.process_filetype(filetype)
+    # Most defaults are explicitly set to None
+    # Setting fallback defaults is instead delayed until after
+    # argparse is complete so that:
+    # 1. We can detect and explicitly log this behaviour
+    # 2. Make more complex decisions about what the default should be
+    parser.add_argument(
+        '--debug',
+        help='enable debug', action='store_true'
+    )
+    parser.add_argument(
+        '--quiet',
+        help='quiet mode - will only output warnings or errors', action='store_true'
+    )
+    parser.add_argument(
+        '--silent',
+        help='silent mode - will output nothing', action='store_true'
+    )
+    parser.add_argument(
+        '--usage',
+        help='provide command specific usage guidance', action='store_true'
+    )
+    parser.add_argument(
+        '--force',
+        help='force through actions - ignoring warnings accepting implicit defaults', action='store_true'
+    )
+    parser.add_argument(
+        '--input',
+        nargs='?',
+        type=str,
+        default=None,
+        help='specify directory where audio files are stored'
+    )
+    parser.add_argument(
+        '--database',
+        nargs='?',
+        type=str,
+        default=None,
+        help='specify the database file to use',
+    )
+    parser.add_argument(
+        'command',
+        nargs='*',
+        type=str,
+        help='run this command',
+        metavar='command',
+        choices=_discover_commands()
+    )
+    return parser.parse_args()
 
-if __name__ == "__main__":
+def _discover_commands():
+    return [module_name.replace('flaccurate.commands.','')
+                for module_name in sys.modules.keys()
+                    if(_valid_command(module_name))]
+
+def _valid_command(command):
+    if( command.startswith('flaccurate.commands.') and
+        command != 'flaccurate.commands.base' ):
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
     main()
